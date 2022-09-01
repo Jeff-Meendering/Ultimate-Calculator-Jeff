2b033537547bab1d7ec6b513f8dc83762bcb83f9
pavel <pavel.maslov01@gmail.com>
2022-09-01 15:49:09 -0400

    made more changes to experimental

---
+++ experimental.py
@@ -0,0 +1,32 @@
+# print("Enter as many numbers as you want to: ")
+#
+# numbers = []
+# counter = 1
+# n = int(input("Enter the amount of numbers that you plan on using: "))
+#
+# # iterating till the range
+# for i in range(0, n):
+#     ele = int(input("Enter your " + F'{counter}' + "st number: "))
+#     counter += 1
+#     numbers.append(ele)  # adding the element
+#
+#
+# print(numbers)
+
+# ^^^^ working prototype
+
+
+# decimal to binary
+
+# binary = input('enter a number: ')
+# decimal = 0
+# for digit in binary:
+#     decimal = decimal * 2 + int(digit)
+#
+# print(decimal)
+
+# binary = input('enter a number: ')
+# result = int(binary, 2)
+# print(result)
+
+#
