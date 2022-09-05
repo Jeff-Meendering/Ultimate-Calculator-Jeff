a8b5e0c5634056e7a275e27106ec22f42aadd8fb
jeff <jsmeendering@gmail.com>
2022-09-05 15:10:39 -0400

    created the stats class, need to fix the exception and figure out a way to get out of the loop.

--- stats.py
+++ stats.py
@@ -6,12 +6,11 @@ print("Please enter your list and type 'stop' when done")
 while True:
     try:
         userInput = float(input())
-        if userInput == "stop":
-            break
         numberList.append(userInput)
-    except:
-        print("Error invalid input. Please enter a valid number")
-        continue
 
-print(userInput)
+#breaks out of the loop while whenever any letter is typed
+    except:
+        break
+
+print(numberList)
 
